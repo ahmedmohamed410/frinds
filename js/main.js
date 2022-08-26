b8b74ba215dfeb012b494715ae9a37fc87602d83
ahmedmohamed410 <109282947+ahmedmohamed410@users.noreply.github.com>
2022-08-27 00:19:52 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let img = document.querySelector('.img');
+let d = document.querySelector('.d');
+let container = document.querySelector('.container');
+function names(name){
+    img.src = name;
+}
+function klm(x){
+    d.style.width(x)
+}
+function colors(color){
+    container.style.background = color;
+}
